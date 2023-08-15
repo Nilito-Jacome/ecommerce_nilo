541cfbc45b14c1f79f677c940ebe6428a420838a
Nilo-Jacome <nilitojacome@gmail.com>
2023-08-14 23:32:14 -0500

    ecommerce_nilo
    actualizacion

--- src/config/config.js
+++ src/config/config.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: "postgres",
-    password: "1978",
-    database: "ecommerce_db_nj",
-    port: 5432,
-    host: "localhost",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
     dialect: "postgres",
     logging: false,
   },
